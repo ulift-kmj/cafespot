cf239fe417f3fe184e2dc6e0b9918f470f6e5562
ulift-kmj <kmj0107@ulift.inc>
2025-06-23 12:40:13 +0900

    refactor: Navbar를 Header로 이름 변경

--- src/components/Layout/Header/HeaderContainer.tsx
+++ src/components/Layout/Header/HeaderContainer.tsx
@@ -1,8 +1,8 @@
-interface NavbarContainerProps {
+interface HeaderContainerProps {
   children: React.ReactNode;
 }
 
-export default function NavbarContainer({ children }: NavbarContainerProps) {
+export default function HeaderContainer({ children }: HeaderContainerProps) {
   return (
     <div className="w-full px-4 sm:px-6 lg:px-8 border-b-2">
       <div className="w-[90%] mx-auto">{children}</div>
