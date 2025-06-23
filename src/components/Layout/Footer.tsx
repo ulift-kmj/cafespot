b778ab47873b9345b8096b1de3b550569767f5e2
ulift-kmj <kmj0107@ulift.inc>
2025-06-23 12:40:30 +0900

    feat: Footer 컴포넌트 구현

---
+++ src/components/Layout/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 py-8">
+      <p className="text-center text-gray-600">
+        © 2025 CafeSpot. All rights reserved.
+      </p>
+    </footer>
+  );
+}
