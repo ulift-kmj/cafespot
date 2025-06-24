19faf63b93b226ee331c7b4a91c3debdba549595
ulift-kmj <kmj0107@ulift.inc>
2025-06-24 16:17:09 +0900

    feat: React Query 및 라우터 설정 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import '@/index.css';
+import { queryClient } from '@/lib/queryClient';
+import { router } from '@/routes';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
+);
